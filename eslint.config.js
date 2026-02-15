400ff07f3af471948c1f7bae22e9fd6ce65aca84
zachtschetter <zachary.tschetter@usd.edu>
2026-02-14 18:14:21 -0600

    added some settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,13 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
-module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
+module.exports = {
+  extends: ["expo"],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "off",
+    "unused-imports/no-unused-vars": "off",
   },
-]);
+};
