8ea760d3d0003b480dbce5cfc7864a12ae5c3d4a
zachtschetter <zachary.tschetter@usd.edu>
2026-02-14 18:36:02 -0600

    eslint defaults

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,9 @@
 const { defineConfig } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 
-module.exports = {
-  extends: ["expo"],
-  rules: {
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "off",
-    "unused-imports/no-unused-vars": "off",
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ["dist/*"],
   },
-};
+]);
