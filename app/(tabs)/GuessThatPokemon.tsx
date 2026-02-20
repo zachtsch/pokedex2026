5bdacf47daa885cbe2483bd33882e017b2603912
EmilyK-USD <emily.krommendyk@coyotes.usd.edu>
2026-02-19 20:55:42 -0600

    added Guess That Pokemon tab, added button

---
+++ app/(tabs)/GuessThatPokemon.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { View, Image, Button, StyleSheet } from 'react-native';
+
+export default function GuessThatPokemon() {
+  const [isGuessed, setIsGuessed] = useState(false);
+
+  // For future, we'll use url from evolution team. [id].tsx
+  const pokemonImageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png';
+
+  return (
+    <View style={styles.container}>
+      <Image
+        key={isGuessed ? 'reveal' : 'hide'}
+        source={{ uri: pokemonImageUrl }}
+        style={[
+          styles.image,
+          !isGuessed && { tintColor: 'black' }
+        ]}
+      />
+
+      <Button
+        title="Guess Pokemon"
+        onPress={() => {}}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+});
